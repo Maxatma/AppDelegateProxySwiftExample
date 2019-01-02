139dc5c0edaeb966c21871d1fde7d29982279217
Oleksandr Zaporozhchenko <o.zaporozhchenko@mobilunity.com>
2019-01-02 17:03:11 +0200

    Initial Commit

---
+++ MinimalAppDelegateTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MinimalAppDelegateTest
+//
+//  Created by Oleksandr Zaporozhchenko on 1/2/19.
+//  Copyright © 2019 Oleksandr Zaporozhchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
